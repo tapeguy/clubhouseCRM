cd7b3194911f14824f2021b1da75f0c859eed9f3
ss047890 <Scott.Steele@Cerner.com>
2017-08-26 19:24:44 -0500

    add spring starter project + .gitignore

---
+++ src/main/java/edu/fhsu/csci466/clubhouse/crm/ClubhouseCRMApplication.java
@@ -0,0 +1,22 @@
+package edu.fhsu.csci466.clubhouse.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ss047890
+ *
+ *         The entry point to the ClubhouseCRM application.
+ */
+@SpringBootApplication
+public class ClubhouseCRMApplication
+{
+
+    /**
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        SpringApplication.run( ClubhouseCRMApplication.class, args );
+    }
+}
