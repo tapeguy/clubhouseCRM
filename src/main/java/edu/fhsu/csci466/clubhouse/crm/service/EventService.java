b87d55f30dccebc40124dd4e7d4dd494a676b4f9
ss047890 <scott.steele@cerner.com>
2017-12-09 22:01:34 -0600

    impl addMemberToEvent

--- src/main/java/edu/fhsu/csci466/clubhouse/crm/service/EventService.java
+++ src/main/java/edu/fhsu/csci466/clubhouse/crm/service/EventService.java
@@ -8,34 +8,40 @@ import edu.fhsu.csci466.clubhouse.crm.service.model.services.Event;
  * @author ss047890
  *
  */
-public interface EventService
-{
-    /**
-     * @param event
-     * @return whether event creation was successful
-     */
-    public boolean addEvent( Event event );
+public interface EventService {
+	/**
+	 * @param event
+	 * @return whether event creation was successful
+	 */
+	public boolean addEvent(Event event);
 
-    /**
-     * @return event
-     */
-    public List<Event> getEvents();
+	/**
+	 * @return event
+	 */
+	public List<Event> getEvents();
 
-    /**
-     * @param id the id of the event to get
-     * @return event
-     */
-    public Event getEvent( final Long id );
+	/**
+	 * @param id the id of the event to get
+	 * @return event
+	 */
+	public Event getEvent(final Long id);
 
-    /**
-     * @param event
-     * @return whether event update was successful
-     */
-    public boolean updateEvent( Event event );
+	/**
+	 * @param event
+	 * @return whether event update was successful
+	 */
+	public boolean updateEvent(Event event);
 
-    /**
-     * @param id the id of the event to delete
-     * @return whether event deletion was successful
-     */
-    public boolean deleteEvent( final Long id );
+	/**
+	 * @param id the id of the event to delete
+	 * @return whether event deletion was successful
+	 */
+	public boolean deleteEvent(final Long id);
+
+	/**
+	 * @param memberId the id of the member to add to the event
+	 * @param id the id of the event to which to add the member
+	 * @return whether the member was successfully added to the event
+	 */
+	public boolean addMemberToEvent(Long memberId, Long id);
 }
--- src/main/java/edu/fhsu/csci466/clubhouse/crm/service/JPAEventService.java
+++ src/main/java/edu/fhsu/csci466/clubhouse/crm/service/JPAEventService.java
@@ -1,5 +1,6 @@
 package edu.fhsu.csci466.clubhouse.crm.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Service;
 
+import edu.fhsu.csci466.clubhouse.crm.service.model.Member;
 import edu.fhsu.csci466.clubhouse.crm.service.model.services.Event;
 import edu.fhsu.csci466.clubhouse.crm.service.repo.EventRepository;
+import edu.fhsu.csci466.clubhouse.crm.service.repo.MemberRepository;
 
 /**
  * @author ss047890
@@ -17,63 +20,89 @@ import edu.fhsu.csci466.clubhouse.crm.service.repo.EventRepository;
 @Service
 @EnableJpaRepositories
 @EntityScan
-public class JPAEventService implements EventService
-{
-    private final EventRepository eventRepo;
+public class JPAEventService implements EventService {
+	private final EventRepository eventRepo;
+	private final MemberRepository memberRepo;
 
-    /**
-     * @param eventRepo
-     */
-    @Autowired
-    public JPAEventService ( EventRepository eventRepo )
-    {
-        this.eventRepo = eventRepo;
-    }
+	/**
+	 * @param eventRepo
+	 */
+	@Autowired
+	public JPAEventService(EventRepository eventRepo, MemberRepository memberRepo) {
+		this.eventRepo = eventRepo;
+		this.memberRepo = memberRepo;
+	}
 
-    @Override
-    public boolean addEvent( Event event )
-    {
-        if ( event != null )
-        {
-            eventRepo.save( event );
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean addEvent(Event event) {
+		if (event != null) {
+			eventRepo.save(event);
+			return true;
+		}
+		return false;
+	}
 
-    @Override
-    public List<Event> getEvents()
-    {
-        List<Event> events = eventRepo.findAll();
-        return events;
-    }
+	@Override
+	public List<Event> getEvents() {
+		List<Event> events = eventRepo.findAll();
+		return events;
+	}
 
-    @Override
-    public Event getEvent( final Long id )
-    {
-        return eventRepo.findOne( id );
-    }
+	@Override
+	public Event getEvent(final Long id) {
+		return eventRepo.findOne(id);
+	}
 
-    @Override
-    public boolean updateEvent( Event event )
-    {
-        // ensure event already exists before trying to update
-        if ( event != null && eventRepo.exists( event.getEventId() ) )
-        {
-            eventRepo.save( event );
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean updateEvent(Event event) {
+		// ensure event already exists before trying to update
+		if (event != null && eventRepo.exists(event.getEventId())) {
+			eventRepo.save(event);
+			return true;
+		}
+		return false;
+	}
 
-    @Override
-    public boolean deleteEvent( final Long id )
-    {
-        if ( id != null && eventRepo.exists( id ) )
-        {
-            eventRepo.delete( id );
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean deleteEvent(final Long id) {
+		if (id != null && eventRepo.exists(id)) {
+			eventRepo.delete(id);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean addMemberToEvent(Long memberId, Long id) {
+		if (memberId != null && id != null) {
+			Event e = eventRepo.getOne(id);
+			Member m = memberRepo.getOne(memberId);
+			if (m != null && e != null) {
+				memberRepo.save(addEventToMember(m, e));
+				eventRepo.save(addMemberToEvent(m, e));
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static Event addMemberToEvent(final Member m, final Event e) {
+		Event event = e;
+		List<Member> members = event.getMembers();
+		if (members == null)
+			members = new ArrayList<>();
+		members.add(m);
+		event.setMembers(members);
+		return event;
+	}
+
+	private static Member addEventToMember(final Member m, final Event e) {
+		Member member = m;
+		List<Event> events = m.getMemberEvents();
+		if (events == null)
+			events = new ArrayList<>();
+		events.add(e);
+		m.setMemberEvents(events);
+		return member;
+	}
 }
